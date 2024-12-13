56387de233d6f021d982e5c6d82f4248689f950e
horiguchi <horiguchiko@gmail.com>
2024-12-14 01:45:53 +0900

    Format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-import js from '@eslint/js'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser' // パーサをimport
-import nextPlugin from '@next/eslint-plugin-next'
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser'; // パーサをimport
+import nextPlugin from '@next/eslint-plugin-next';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-const { configs: jsConfigs } = js
-const { configs: tsConfigs } = tsPlugin
+const { configs: jsConfigs } = js;
+const { configs: tsConfigs } = tsPlugin;
 
 export default [
   {
@@ -16,22 +16,22 @@ export default [
       parser: tsParser,
       parserOptions: {
         project: './tsconfig.json',
-        sourceType: 'module'
+        sourceType: 'module',
       },
       globals: {
-        console: 'readonly'
+        console: 'readonly',
       },
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
-      'prettier': prettierPlugin,
-      '@next/next': nextPlugin
+      prettier: prettierPlugin,
+      '@next/next': nextPlugin,
     },
     rules: {
       ...jsConfigs.recommended.rules,
       ...tsConfigs.recommended.rules,
       ...prettierConfig.rules,
-      'prettier/prettier': 'error'
-    }
-  }
-]
+      'prettier/prettier': 'error',
+    },
+  },
+];
