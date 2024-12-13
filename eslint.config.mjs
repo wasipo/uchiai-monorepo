2cbdcb3a8daf636f47a6d054b8de96012b25d11f
horiguchi <horiguchiko@gmail.com>
2024-12-14 01:39:35 +0900

    静的解析を導入

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser' // パーサをimport
+import nextPlugin from '@next/eslint-plugin-next'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+const { configs: jsConfigs } = js
+const { configs: tsConfigs } = tsPlugin
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['node_modules', 'dist', '.next', 'out'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
+      },
+      globals: {
+        console: 'readonly'
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'prettier': prettierPlugin,
+      '@next/next': nextPlugin
+    },
+    rules: {
+      ...jsConfigs.recommended.rules,
+      ...tsConfigs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error'
+    }
+  }
+]
